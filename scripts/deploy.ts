d58368438b4a1c3ee9dcae6612899a495d8f5c3b
Ilya Kubariev <ilya.kubariev@redduck.io>
2022-09-07 19:34:21 +0300

    feat: new scripts

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -5,11 +5,14 @@
 // Runtime Environment's members available in the global scope.
 import { ethers } from 'hardhat';
 
+const BANNED_ADDRESS = '0xdA0Fb2305EEad572fEAe4ee8C058C75760D49154';
+
 async function main() {
   const [deployer] = await ethers.getSigners();
   const factory = await ethers.getContractFactory('SignatureVerify', deployer);
   const verifySignature = await (await factory.deploy()).deployed();
-  console.log(verifySignature.address);
+  await (await verifySignature.addBlacklistedContract(BANNED_ADDRESS)).wait();
+  await console.log(verifySignature.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
