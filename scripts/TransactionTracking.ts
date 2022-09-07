820ea34d1d8bdeb0489e3dd04ec284dbf663485d
Ilya Kubariev <ilya.kubariev@redduck.io>
2022-09-07 19:22:57 +0300

    feat: new scripts

--- scripts/TransactionTracking.ts
+++ scripts/TransactionTracking.ts
@@ -1,7 +1,54 @@
-// @ts-nocheck
-import { expect } from 'chai';
 import type { ContractTransaction } from 'ethers';
 import { ethers } from 'hardhat';
+
+const wsProvider = new ethers.providers.WebSocketProvider(
+  'wss://goerli.infura.io/ws/v3/a962089e87a943da93eb2d06c8b6fc35',
+  'goerli',
+);
+// https://goerli.infura.io/v3/a962089e87a943da93eb2d06c8b6fc35
+
+const VERIFY_SIGNATURE_ADDRESS = '0xDE5C099d596cA9f1EA6171162f55a0Fc01ef6343';
+const FORBIDDEN_ADDRESSES = [
+  '0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D',
+  '0x00000000006c3852cbEf3e08E8dF289169EdE581',
+  '0xdA0Fb2305EEad572fEAe4ee8C058C75760D49154',
+];
+
+console.log('Script started.');
+
+wsProvider.on('block', async (blockNumber) => {
+  console.log('Found new block.');
+  const blockWithTxs = await wsProvider.getBlockWithTransactions(blockNumber);
+
+  await Promise.all(
+    blockWithTxs.transactions
+      .filter((tx) => tx.to && FORBIDDEN_ADDRESSES.includes(tx.to))
+      .map(async (tx) => {
+        // some actions to contracts blockUser
+        console.log('Found tx to forbidden address.');
+
+        const expandedSig = ethers.utils.joinSignature({
+          // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+          r: tx.r!,
+          s: tx.s,
+          v: tx.v,
+        });
+
+        const signature = ethers.utils.joinSignature(expandedSig);
+        const serializedTx = await getSerializedTx(getTxData(tx));
+
+        const [signer] = await ethers.getSigners();
+        console.log(`Signer: ${signer.address}`);
+        const verifySignature = await ethers.getContractAt(
+          'SignatureVerify',
+          VERIFY_SIGNATURE_ADDRESS,
+          signer,
+        );
+        await (await verifySignature.report(serializedTx, signature)).wait();
+        console.log('signature', signature);
+      }),
+  );
+});
 
 const getTxData = (tx: ContractTransaction) => {
   switch (tx.type) {
@@ -37,38 +84,3 @@ const getSerializedTx = async (txData: ReturnType<typeof getTxData>) =>
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     await ethers.utils.resolveProperties(<any>txData),
   );
-
-var wsProvider = new ethers.providers.WebSocketProvider("wss://mainnet.infura.io/ws/v3/a962089e87a943da93eb2d06c8b6fc35", "mainnet");
-//https://mainnet.infura.io/v3/a962089e87a943da93eb2d06c8b6fc35
-
-  wsProvider.on('block', (blockNumber) => {
-
-  async function getTxData(blockNumber: number) {
-    const blockWithTxs = await wsProvider.getBlockWithTransactions(blockNumber)
-
-    let txLengh = blockWithTxs.transactions.length
-    let forbiddenAddresses = ['0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D', '0x00000000006c3852cbEf3e08E8dF289169EdE581']
-
-    await Promise.all(blockWithTxs.transactions.filter((tx) => forbiddenAddresses.includes(tx.to)).map(async (tx) => {
-      //some actions to contracts blockUser
-      const signature = ethers.utils.joinSignature({
-        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-        r: tx.r!,
-        s: tx.s,
-        v: tx.v,
-      });
-
-      const signature = ethers.utils.joinSignature(expandedSig)
-      const serializedTx = await getSerializedTx(getTxData(tx));
-
-      await (await verifySignature.report(serializedTx, signature)).wait()
-      console.log('signature', signature)
-
-    }))
-
-  }
-  //14752527216
-  // console.log('newbLOCK')
-  getTxData(blockNumber)
-});
-
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -6,23 +6,10 @@
 import { ethers } from 'hardhat';
 
 async function main() {
-  // Hardhat always runs the compile task when running scripts with its command
-  // line interface.
-  //
-  // If this script is run directly using `node` you may want to call compile
-  // manually to make sure everything is compiled
-  // await hre.run('compile');
-
-  // We get the contract to deploy
-  const Greeter = await ethers.getContractFactory('Greeter');
-  const greeter = await Greeter.deploy('Hello, Hardhat!');
-
-  await greeter.deployed();
-
-  const tx = await greeter.setGreeting('HELLO2').then(i => i.wait());
-
-  console.log("tx: ", tx);
-
+  const [deployer] = await ethers.getSigners();
+  const factory = await ethers.getContractFactory('SignatureVerify', deployer);
+  const verifySignature = await (await factory.deploy()).deployed();
+  console.log(verifySignature.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
